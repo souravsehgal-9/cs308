0d54cdc28b9535d9e47b8e052840c1bfeb3c6e70
Sourav Sehgal <souravsehgal2001@gmail.com>
2021-09-25 12:12:12 +0530

    Done Successfully

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int main()
     cout << endl;
     cout << "The factorial of 11 is " << factorial(11) << endl;
     int x = 56;
-    int y = 98;
+    int y = 92;
     cout << "The gcd of " << x << " " << y << " is " << gcd(x, y) << endl;
 }

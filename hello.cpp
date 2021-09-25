82c7a69e44a077e819c34985355c44e841fe5645
Sourav Sehgal <souravsehgal2001@gmail.com>
2021-09-25 13:23:40 +0530

    moving

--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,7 @@
 #include "functions.h"
 using namespace std;
 void print_hello(){
-   cout<<"Hello Sourav Sehgal!\nHope you are fine";
+   printf("\n");
+   prinf("Hello Sourav Sehgal!\nHope you are fine");
+   printf("\n");
 }

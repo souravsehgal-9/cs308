9c42d1394fda58131a893aad4ffa6a48a3df3a5e
Sourav Sehgal <souravsehgal2001@gmail.com>
2021-09-25 11:26:44 +0530

    Init

--- hello.cpp
+++ hello.cpp
@@ -2,5 +2,5 @@
 #include "functions.h"
 using namespace std;
 void print_hello(){
-   cout<<"Hello Sourav!";
+   cout<<"Hello Sourav Sehgal!";
 }
--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
     printf("\n");
     print_hello();
     cout << endl;
-    cout << "The factorial of 5 is " << factorial(5) << endl;
+    cout << "The factorial of 11 is " << factorial(11) << endl;
     int x = 56;
     int y = 98;
     cout << "The gcd of " << x << " " << y << " is " << gcd(x, y) << endl;
